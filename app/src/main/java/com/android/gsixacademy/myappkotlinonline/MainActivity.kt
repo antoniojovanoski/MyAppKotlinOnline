700e179ade2f997c30d086b05fedc981811fc134
antoniojovanoski <jovanoskiantonio1998@gmail.com>
2020-04-27 20:33:38 +0200

    initial commit

--- app/src/main/java/com/android/gsixacademy/myappkotlinonline/MainActivity.kt
+++ app/src/main/java/com/android/gsixacademy/myappkotlinonline/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-var listActivities: ArrayList<String> = arrayListOf("RelativeLayoutActivity","CalculatorActivity","TodoListActivity","RecyclerViewActivity")
+var listActivities: ArrayList<String> = arrayListOf("RelativeLayoutActivity","CalculatorActivity","TodoListActivity","RecyclerViewActivity","LoginActivity")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Picasso.get().load("https://tinyjpg.com/images/social/website.jpg").fit().into(image_view_background)
+
 
         var activitiesAdapter: ArrayAdapter<String> =  ArrayAdapter(this,android.R.layout.simple_list_item_1,listActivities)
         list_view_activities.adapter = activitiesAdapter
@@ -28,6 +32,7 @@ var listActivities: ArrayList<String> = arrayListOf("RelativeLayoutActivity","Ca
     "TodoListActivity" -> startActivity(Intent(applicationContext, TodoListActivity::class.java))
 
     "RecyclerViewActivity" -> startActivity(Intent(applicationContext, RecyclerViewActivity::class.java))
+               "LoginActivity" -> startActivity(Intent(applicationContext, LoginActivity::class.java))
 }
 
             Toast.makeText(applicationContext,className,Toast.LENGTH_LONG).show()
---
+++ app/src/main/java/com/android/gsixacademy/myappkotlinonline/UserSignedInActivity.kt
@@ -0,0 +1,23 @@
+package com.android.gsixacademy.myappkotlinonline
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_user_signed_in.*
+
+class UserSignedInActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_signed_in)
+
+       var mySharedPreferences = getSharedPreferences("KotlinBasicTutorialPreferences", Context.MODE_PRIVATE)
+        var Myusername = mySharedPreferences.getString("userName","///")
+        text_view_title.text = "Signed in user is $Myusername"
+
+        button_log_out.setOnClickListener {
+            mySharedPreferences.edit().remove("$Myusername")
+            onBackPressed()
+
+        }
+    }
+}
